8eec30c095308467ca19e3599429413530b635bc
Peter Lauck <griest024@gmail.com>
2015-04-23 01:45:14 -0400

    progress commit on dynamic modification of weather

--- src/Scripts/Scripts/Sandbox.rb
+++ src/Scripts/Scripts/Sandbox.rb
@@ -2,6 +2,7 @@ class DebugTerrainTags
   
   # testingzsdgfdsasdfds
   
+  
   def initialize
     echo("Terrain tag: ")
     echoln(pbGetTerrainTag)
@@ -18,5 +19,71 @@ class DebugTerrainTags
   end
   
 end
+
+class DebugWeather
+
+    # class MapMetadata
+    #     line_number_start = 0   #the line number of the map id
+    #     line_number_end = 0   #the line number  before the next map id
+    #     id = nil     
+    #     init = false             
+
+    #     def initialize(map_id)
+    #         id = map_id
+    #         echoln(id)
+    #         init = findMap(map_id)
+    #         puts init
+    #     end
+
+    #     def isMap?(line)
+    #         return line == "[" + id + "]"
+    #     end
+
+    #     def findMap(map_id)
+    #     File.open("PBS/metadata.txt", 'r+') do |f|
+    #       echo(f)
+    #      f.each_line do |line|
+    #         line_number_start++
+    #         unless isMap?(line)
+    #             echoln(line_number_start + line + "not yet")
+    #         else
+    #             return true
+    #         end
+    #      end
+    #      return false
+    #     end
+    # end
+######################################################################
+
+
+id = 0
+map_selected = nil
+
+
+    def initialize
+      
+    end
+
+    
+  def selectMap(map_id)
+    map_selected = MapMetadata.new(map_id)
+  end
+  
+  def setWeather(weather_id)
+    if map_selected.init
+    end
+  end
+  
+    
+    def getMetadata(map_id)
+      return pbGetMetadata(map_id,MetadataWeather)
+    end
+    
+      
+        
+
+
+end
+
 
     
