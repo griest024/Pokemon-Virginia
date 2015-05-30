c46dfe0d04a329a23d47484055814745aa403e22
Caroline <csakura760@gmail.com>
2015-05-30 10:55:31 -0400

    worked on the new town map

--- Game.bat
+++ Game.bat
@@ -7,7 +7,7 @@ REM ========================================================
 
 REM # The plugin system directory path relative to where this
 REM # file resides (RMXP project root).
-SET PLUGIN_SYSTEM_ROOT="..\\PKMNEssentialsPlugin"
+SET PLUGIN_SYSTEM_ROOT="..\\pokemon plugin"
 
 REM # The RMXP project root path relative to the plugin
 REM # system's root.
